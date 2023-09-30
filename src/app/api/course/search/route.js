3158ed1a1906dba05c265422197ac04ffd293115
JEXB <jippee.1211@gmail.com>
2023-09-30 17:14:36 +0700

    Lab22

--- src/app/api/course/search/route.js
+++ src/app/api/course/search/route.js
@@ -4,9 +4,17 @@ import { NextResponse } from "next/server";
 export const GET = async (request) => {
   const searchText = request.nextUrl.searchParams.get("searchText");
   const prisma = getPrisma();
-
-  //Modify following line so that it find course with "searchText" variable
-  const courses = await prisma.course.findMany({});
+  const courses = await prisma.course.findMany({
+    where: {
+      title: {
+        contains: searchText,
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      courseNo: "asc",
+    },
+  });
 
   return NextResponse.json({ ok: true, courses });
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="630610657"
+                fullName="suwichada pongkanmoon"
                 year="2023"
               />
             </Container>
